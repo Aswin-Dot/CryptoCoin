0ccbcbe8f409b390928f1b81e0dc5054ece4322d
Aswin Raj <56764210+Aswin-Dot@users.noreply.github.com>
2021-12-14 20:00:23 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { CryptoDetail, Transaction } from "./src/screens";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from "@expo-google-fonts/roboto";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Tabs from "./src/navigations/tabs";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName={"Home"}
+        >
+          <Stack.Screen name="Tabs" component={Tabs} />
+          <Stack.Screen name="CryptoDetail" component={CryptoDetail} />
+          <Stack.Screen name="Transaction" component={Transaction} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+};
+
+export default App;
