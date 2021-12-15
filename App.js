bf723878f910ce8c884dddd4fc540ea48d24167d
Aswin Raj <56764210+Aswin-Dot@users.noreply.github.com>
2021-12-15 13:40:22 +0530

    bottom tab nav

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import { CryptoDetail, Transaction } from "./src/screens";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
 
 import {
   useFonts,
@@ -22,11 +22,47 @@ const App = () => {
     Roboto_900Black,
   });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady || !fontsLoaded) {
+    return null;
   } else {
     return (
-      <NavigationContainer>
+      <NavigationContainer onReady={onLayoutRootView}>
         <Stack.Navigator
           screenOptions={{
             headerShown: false,
--- src/navigations/tabs.js
+++ src/navigations/tabs.js
@@ -6,7 +6,8 @@ import {
     Text,
     StyleSheet
 } from "react-native";
-import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs"
+import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs";
+import { LinearGradient } from "expo-linear-gradient";
 
 import { Home } from "../screens"
 import { COLORS, FONTS, icons } from "../constants"
@@ -14,30 +15,202 @@ import { COLORS, FONTS, icons } from "../constants"
 const Tab = createBottomTabNavigator()
 
 const Tabs = () => {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name="Home"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Portfolio"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Transaction"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Prices"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Settings"
-                component={Home}
-            />
-        </Tab.Navigator>
+
+    const TabBarCustomButton = ({ children, onPress }) => (
+        <TouchableOpacity 
+            style={{
+                top: -35,
+                alignItems: "center",
+                justifyContent: "center",
+                ...styles.shadow
+            }}
+            onPress={onPress}
+        >
+            <LinearGradient
+                colors={[COLORS.primary, COLORS.secondary]}
+                style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 35
+            }}>
+                {children}
+            </LinearGradient>
+            
+
+        </TouchableOpacity>
     )
+
+    return (
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            elevation: 0,
+            backgroundColor: COLORS.white,
+            height: "10%",
+            borderTopColor: "transparent",
+          },
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "flex-start",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={icons.home}
+                  resizeMode="contain"
+                  style={{
+                    height: 30,
+                    width: 30,
+                    tintColor: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                />
+                <Text
+                  style={{
+                    ...FONTS.body5,
+                    color: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                >
+                  Home
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Portfolio"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "flex-start",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={icons.pie_chart}
+                  resizeMode="contain"
+                  style={{
+                    height: 30,
+                    width: 30,
+                    tintColor: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                />
+                <Text
+                  style={{
+                    ...FONTS.body5,
+                    color: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                >
+                  Portfolio
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Transaction"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={icons.transaction}
+                resizeMode="contain"
+                style={{
+                  height: 30,
+                  width: 30,
+                  tintColor: COLORS.white,
+                }}
+              />
+            ),
+            tabBarButton: (props) => (
+                <TabBarCustomButton {...props}/>
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Prices"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "flex-start",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={icons.line_graph}
+                  resizeMode="contain"
+                  style={{
+                    height: 30,
+                    width: 30,
+                    tintColor: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                />
+                <Text
+                  style={{
+                    ...FONTS.body5,
+                    color: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                >
+                  Prices
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "flex-start",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={icons.settings}
+                  resizeMode="contain"
+                  style={{
+                    height: 30,
+                    width: 30,
+                    tintColor: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                />
+                <Text
+                  style={{
+                    ...FONTS.body5,
+                    color: focused ? COLORS.primary : COLORS.gray,
+                  }}
+                >
+                  Profile
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
 }
 
 const styles = StyleSheet.create({
